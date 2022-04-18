b5b3d9613f39726311387c6a53567360aa3af464
Gileaddias <gfarias.gpf@gmail.com>
2022-04-18 16:39:32 -0300

    Criando novos estilos CSS para o game

--- scripts.js
+++ scripts.js
@@ -1,4 +1,107 @@
-let order = [];
+const _data = {
+	gameOn: false,
+	timeout: undefined,
+	sounds: [],
+
+	strict: false,
+	playerCanPlay: false,
+	score: 0,
+	gameSequence: [],
+	playerSequence: []
+};
+
+const _gui = {
+	counter: document.querySelector(".gui__counter"),
+	switch: document.querySelector(".gui__btn-switch"),
+	led: document.querySelector(".gui__led"),
+	strict: document.querySelector(".gui__btn--strict"),
+	start: document.querySelector(".gui__btn--start"),
+	pads: document.querySelectorAll(".game__pad")
+}
+
+const _soundUrls = [
+	"audio/simonSound1.mp3",
+	"audio/simonSound2.mp3",
+	"audio/simonSound3.mp3",
+	"audio/simonSound4.mp3"
+];
+
+_soundUrls.forEach(sndPath => {
+	const audio = new Audio(sndPath);
+	_data.sounds.push(audio);
+});
+
+_gui.switch.addEventListener("click", () => {
+
+});
+
+_gui.strict.addEventListener("click", () => {
+
+});
+
+_gui.start.addEventListener("click", () => {
+
+});
+
+const padListener = (e) => {
+
+}
+
+_gui.pads.forEach(pad => {
+	pad.addEventListener("click", padListener);
+});
+
+const startGame = () => {
+
+}
+
+const setScore = () => {
+
+}
+
+const newColor = () => {
+
+}
+
+const playSequence = () => {
+
+}
+
+const blink = (text, callback) => {
+
+}
+
+const waitForPlayerClick = () => {
+
+}
+
+const resetOrPlayAgain = () => {
+
+}
+
+const changePadCursor = (cursorType) => {
+
+}
+
+const disablePads = () => {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+/***********************
+ * ******************** */
+/*let order = [];
 let clickedOrder = [];
 let score = 0;
 
@@ -104,7 +207,7 @@ yellow.addEventListener('click', click(2));
 blue.addEventListener('click', click(3));
 */
 
-//eventos de clique para as cores
+/*/eventos de clique para as cores
 green.onclick = () => click(0);
 red.onclick = () => click(1);
 yellow.onclick = () => click(2);
@@ -112,4 +215,4 @@ blue.onclick = () => click(3);
 
 //inicio do jogo
 playGame();
-
+*/
